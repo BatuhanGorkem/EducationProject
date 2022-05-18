80dfe3e77f4dcd182df1205f3550129259881fd8
Batuhan Gorkem <batuhangorkem_8@hotmail.com>
2022-05-18 21:06:18 +0300

    statik dosyalar düzenlendi

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const app = express();
-
+app.set("view engine", "ejs");
 const port = 3000;
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.status(200).send("INDEX SAYFASI");
+  res.status(200).render("index");
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about");
 });
 
 app.listen(port, () => {
